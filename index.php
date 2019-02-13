ad6155cefc726b7258a808a61ee01942e9951350
gpremchand <34093786+gpremchand@users.noreply.github.com>
2019-02-14 00:26:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 i<html>
  <head>
-  <title> PHP Test </title>
+  <title> Premchand Gupta My First PHP </title>
  </head>
  <body>
  <?php echo '<p> Hello World </p>'; ?>
- </body>
+  <marquee> <h4> Well Come to My 1st PHP page </marquee>
+  </body>
 </html>
